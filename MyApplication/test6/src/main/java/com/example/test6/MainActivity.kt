ea4ad5c61f53876557e35e54dd48d28d179a51cf
Pling3244 <okmijndkfwl@nate.com>
2023-05-12 09:13:56 +0900

    test

---
+++ MyApplication/test6/src/main/java/com/example/test6/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.test6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.example.test6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() { // AppCompatActivity 부모 클래스로부터 상속 받음
+    // 코드 작업 공간, 뷰 작업도 가능하지만, 가급적 뷰는 xml에서 작업하기.
+    // 이유? -> 여기에서는 데이터 관련 작업 및 이벤트 핸들러 등 작업하기 위해서, 분리.
+    // 만약, 한 파일에 뷰까지 더해서, 작업을 하게 되면, 가독성이 안좋음. 유지보수 어려움.
+    override fun onCreate(savedInstanceState: Bundle?) { //onCreate 액티비티 생명주기
+        super.onCreate(savedInstanceState) // savedInstanceState -> 저장소 개념. 객체저장.
+        // res -> layout -> xml 파일을 불러와서, 실제로 화면 출력
+        // 리소스 R.java 파일이라는 곳에 상수 값으로 저장되는데, 여기서 불러옵니다.
+        // 그레서, 코드에서 리소스의 값을 불러올 때 항상. R.layout 등으로 시작을 해서 불러옵니다.
+        // 화면을 출력하겠다. 레이아웃(뷰 그룹 : 뷰(텍스트, 이미지, 인풋, 체크박스, 라디오..)를 모아주는 역할
+        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var status = 0
+
+        binding.btn1.setOnClickListener {
+
+            if(status == 0) {
+                binding.img1.visibility = View.INVISIBLE
+                status = 1
+            } else {
+                binding.img1.visibility = View.VISIBLE
+                status = 0
+            }
+
+        }
+
+        val button1 = findViewById<Button>(R.id.btn1)
+        val img1 = findViewById<ImageView>(R.id.img1)
+
+    }
+}
